55084d97adb5dbcf1946c4ae7cf4d34954274b49
Nikhil <aminnikhil073@gmail.com>
2021-11-11 18:33:20 +0530

    Adding query constant

---
+++ backend/src/main/java/com/hostelmanagement/backend/rooms/dao/constants/QueryConstants.java
@@ -0,0 +1,6 @@
+package com.hostelmanagement.backend.rooms.dao.constants;
+
+public class QueryConstants {
+
+    public final static String GET_ROOMS_LIST = "select * from rooms";
+}
--- backend/src/main/java/com/hostelmanagement/backend/rooms/dao/implementation/RoomsDAOImpl.java
+++ backend/src/main/java/com/hostelmanagement/backend/rooms/dao/implementation/RoomsDAOImpl.java
@@ -2,20 +2,26 @@ package com.hostelmanagement.backend.rooms.dao.implementation;
 
 import com.hostelmanagement.backend.exception.DBException;
 import com.hostelmanagement.backend.rooms.dao.RoomsDAO;
+import com.hostelmanagement.backend.rooms.dao.constants.QueryConstants;
 import com.hostelmanagement.backend.rooms.dto.RoomDTO;
+import com.hostelmanagement.backend.util.ParsingUtil;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class RoomsDAOImpl implements RoomsDAO {
 
+    private JdbcTemplate jdbcTemplate;
+
     public List<RoomDTO> getRoomsList() throws DBException {
         List<RoomDTO> rooms = new ArrayList<RoomDTO>();
         try{
-            
+            List<Map<String, String>> output = ParsingUtil.queryForList(jdbcTemplate, QueryConstants.GET_ROOMS_LIST);
         }catch (DataAccessException dae){
             throw new DBException("[ERROR:DAE] getRoomsList() ", dae);
         }catch (NumberFormatException nfe){
