b4a1c52963b9493b72a7b2d28465c1c77987de8b
Nikhil <aminnikhil073@gmail.com>
2021-11-19 20:20:56 +0530

    Mess Service

--- backend/src/main/java/com/hostelmanagement/backend/mess/service/MessService.java
+++ backend/src/main/java/com/hostelmanagement/backend/mess/service/MessService.java
@@ -8,4 +8,18 @@ import java.util.List;
 public interface MessService {
 
     public List<MessDTO> getMess() throws ServiceException;
+
+	public MessDTO getMessByItemID(int itemID) throws ServiceException;
+
+	public void insertMessList(List<MessDTO> messList) throws ServiceException;
+
+	public void insertMess(MessDTO mess) throws ServiceException;
+
+	public void updateMessByItemID(MessDTO mess, int itemID) throws ServiceException;
+
+	public void deleteMessByItemID(int itemID) throws ServiceException;
+
+	public void deleteAllMess() throws ServiceException;
+    
+    
 }
