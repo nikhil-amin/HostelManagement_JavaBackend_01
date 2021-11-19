94314e6038be9ce9a6eb2e4741fa7866b226debb
Nikhil <aminnikhil073@gmail.com>
2021-11-19 20:27:52 +0530

    Mess DAO

--- backend/src/main/java/com/hostelmanagement/backend/mess/dao/MessDAO.java
+++ backend/src/main/java/com/hostelmanagement/backend/mess/dao/MessDAO.java
@@ -1,11 +1,23 @@
 package com.hostelmanagement.backend.mess.dao;
 
+import java.util.List;
+
 import com.hostelmanagement.backend.exception.DBException;
 import com.hostelmanagement.backend.mess.dto.MessDTO;
-
-import java.util.List;
 
 public interface MessDAO {
 
     public List<MessDTO> getMess() throws DBException;
+
+	public MessDTO getMessByItemID(int itemID) throws DBException;
+
+	public void insertMessList(List<MessDTO> messList) throws DBException;
+
+	public void insertMess(MessDTO mess) throws DBException;
+
+	public void updateMessByItemID(MessDTO mess, int itemID) throws DBException;
+
+	public void deleteMessByItemID(int itemID) throws DBException;
+
+	public void deleteAllMess() throws DBException;
 }
