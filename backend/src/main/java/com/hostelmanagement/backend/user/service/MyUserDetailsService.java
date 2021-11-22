36b7081f088c936c0c11caa18d4c85c10e2b888d
Nikhil <aminnikhil073@gmail.com>
2021-11-22 15:33:55 +0530

    [ADD] Bcrypt Password Encoder to dummy login

--- backend/src/main/java/com/hostelmanagement/backend/user/service/MyUserDetailsService.java
+++ backend/src/main/java/com/hostelmanagement/backend/user/service/MyUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,10 @@ public class MyUserDetailsService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return new User("root", "root123", new ArrayList<>());
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String pw = passwordEncoder.encode("root123");
+		
+		return new User("root", pw, new ArrayList<>());
 	}
 
 }
